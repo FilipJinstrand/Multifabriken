e324043131691e69f85fbef15e4138adb0a0c3b5
ellensdogos <ellen.sdogos@hotmail.com>
2019-10-02 11:58:54 +0200

    huvudmeny

--- Program.cs
+++ Program.cs
@@ -6,7 +6,42 @@ namespace Multifabriken
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hej!");
+            bool fabric = true;
+
+            while(fabric)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("\n - Välkommen till huvudmenyn för Multifabriken AB -");
+                Console.WriteLine(" ");
+
+                Console.WriteLine("[1] Gör en beställning");
+                Console.WriteLine("[2] Se innehållet i min beställning");
+                Console.WriteLine("[0] Avsluta programmet");
+                Console.WriteLine(" ");
+                Console.Write("Välj ett alternativ genom att trycka på den motsvarande siffrans tangent: ");
+                Console.WriteLine(" ");
+
+                int menuChoice = Convert.ToInt32(Console.ReadLine());
+
+                switch(menuChoice)
+                {
+                    case 1:
+                    break;
+
+                    case 2:
+                    break; 
+
+                    case 0: 
+                    Console.Clear();
+                    Console.WriteLine("Programmet avslutas...");
+                    return;
+
+                    default:
+                    Console.WriteLine("Felaktig inmatning");
+                    break;
+
+                }
+            }
         }
     }
 }
