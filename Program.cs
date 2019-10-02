43fb85b238a845a3ab0e498164f64b69adaaae07
ellensdogos <ellen.sdogos@hotmail.com>
2019-10-02 11:01:59 +0200

    test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Multifabriken
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hej!");
         }
     }
 }
